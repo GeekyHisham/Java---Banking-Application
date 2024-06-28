24eacbe57fcdbc878405d315645614bac5bddb32
Hisham Hossain <72519957+GeekyHisham@users.noreply.github.com>
2024-06-27 22:29:06 -0400

    Add files via upload

---
+++ FirstBank.java
@@ -0,0 +1,15 @@
+public class FirstBank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account tim = new Account("Tim Statler", "B00002");
+
+
+        Account beyonce = new Account("Hisham Hossain", "A00001");
+        beyonce.showMenu();
+
+
+    }
+
+}
